f5b0c471d9c7c9e868031b2f5eb5b77978bde895
LeonardoBittencourt97 <leonardobittencourtas@gmail.com>
2026-08-21 12:01:01 -0300

    fix: remove conflicting config/ directory, keep config.py

--- config/__init__.py
+++
@@ -1,5 +0,0 @@
-from .settings import settings, Settings
-from .schemas import ClientConfig
-from .client_loader import ClientLoader
-
-__all__ = ["settings", "Settings", "ClientConfig", "ClientLoader"]
